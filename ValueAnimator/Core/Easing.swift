d423a7adf46edd3a6946d38de9e16e1cd600ff93
brownsoo@6009 <hansune@me.com>
2020-02-12 10:03:43 +0900

    fixed comment

--- ValueAnimator/Core/Easing.swift
+++ ValueAnimator/Core/Easing.swift
@@ -8,9 +8,9 @@
 /// Easing method
 ///
 /// - Parameters:
-///   - t Specifies the current time, between 0 and duration inclusive.
-///   - b Specifies the initial value of the animation property.
-///   - c Specifies the total change in the animation property.
-///   - d Specifies the duration of the motion.
+///   - t: Specifies the current time, between 0 and duration inclusive.
+///   - b: Specifies the initial value of the animation property.
+///   - c: Specifies the total change in the animation property.
+///   - d: Specifies the duration of the motion.
 /// - Returns: The value of the interpolated property at the specified time.
 public typealias Easing = (_ t: Double, _ b: Double, _ c: Double, _ d: Double) -> Double
