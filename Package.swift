5733045410276548b840c010e4f03f37f4ef7458
brownsoo_6009 <hansune@me.com>
2020-09-21 12:33:08 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,10 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
                
         .target(
-            name: "ValueAnimatorExample",
-            dependencies: ["ValueAnimator"]),
-        .testTarget(
-            name: "ValueAnimator-iOSTests",
-            dependencies: ["ValueAnimator-iOS"]),
+            name: "ValueAnimator",
+            dependencies: []),
+//        .testTarget(
+//            name: "ValueAnimator-iOSTests",
+//            dependencies: ["ValueAnimator-iOS"]),
     ]
 )
